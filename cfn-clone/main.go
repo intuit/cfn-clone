dbd5830b4fb5cdc7460d78ec176003050fc6adb9
Tommy Bishop <bishop.thomas@gmail.com>
2014-10-10 14:42:22 -0700

    pretty print parameters for new stack

--- cfn-clone/main.go
+++ cfn-clone/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"sort"
+	"text/tabwriter"
 )
+
+func prettyParameters(params map[string]string) string {
+	var b bytes.Buffer
+	if len(params) > 0 {
+		w := new(tabwriter.Writer)
+		w.Init(&b, 0, 8, 0, '\t', 0)
+
+		keys := []string{}
+		for k := range params {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+
+		b.Write([]byte("The merged parameters are:\n"))
+		for _, k := range keys {
+			fmt.Fprintf(w, "%s \t%s\n", k, params[k])
+		}
+		w.Flush()
+	}
+	return b.String()
+}
 
 func main() {
 	options := parseCliArgs()
@@ -25,7 +49,7 @@ func main() {
 		parameters[k] = v
 	}
 
-	fmt.Printf("Our merged parameters are %v\n", parameters)
+	fmt.Println(prettyParameters(parameters))
 
 	fmt.Println("Going to clone")
 
